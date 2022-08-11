3f37592bf4bb1cd55a957bc1e44c9325641c8c79
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-08-11 19:03:25 -0400

    added doubleValues() function

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // doubleValues
 // Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+function double(num){
+    return num * 2;
+}
+
+function doubleValues(arr){
+    const newArr = [];
+
+    const doubleVal = arr.forEach(function(value){
+        return newArr.push(value * 2);
+    });
+
+    return newArr;
+}
